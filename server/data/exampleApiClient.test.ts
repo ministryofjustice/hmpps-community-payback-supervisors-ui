969c4ef1a3cd4f79801fb74c26f341399e8405b9
dhruv.patel <dhruv.patel@justice.gov.uk>
2025-10-17 09:10:40 +0100

    CPB-167: Replace example URL with community payback api url

--- server/data/exampleApiClient.test.ts
+++ server/data/exampleApiClient.test.ts
@@ -22,7 +22,7 @@ describe('ExampleApiClient', () => {
 
   describe('getCurrentTime', () => {
     it('should make a GET request to /example/time using system token and return the response body', async () => {
-      nock(config.apis.exampleApi.url)
+      nock(config.apis.communityPaybackApi.url)
         .get('/example/time')
         .matchHeader('authorization', 'Bearer test-system-token')
         .reply(200, { time: '2025-01-01T12:00:00Z' })
