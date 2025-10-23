45677c9397834f2f5e2c506e2748d88d0b39de79
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-10-23 14:56:43 +0100

    CPB-201(fix): set up cypress

--- server/@types/shared/models/OffenderDto.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-
-export type OffenderDto = {
-  objectType: 'Not_Found' | 'Limited' | 'Full'
-  crn: string
-}
