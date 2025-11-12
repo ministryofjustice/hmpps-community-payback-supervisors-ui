bf5dbbee98f2644093e216126e1b0af944aba8cd
Dan Liburd <daniel.liburd@gmail.com>
2025-11-12 17:56:43 +0000

    Introduce AppointmentService

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,16 @@
 import { dataAccess } from '../data'
+import AppointmentService from './appointmentService'
 import AuditService from './auditService'
 import SessionService from './sessionService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, sessionClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, sessionClient, appointmentClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     sessionService: new SessionService(sessionClient),
+    appointmentService: new AppointmentService(appointmentClient),
   }
 }
 
