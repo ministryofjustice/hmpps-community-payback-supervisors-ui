625e1aac057a275c4aa02f9a9d8cf30cee0fea8f
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-12 11:55:45 +0000

    Add Dto Faker factories
    These are used to create instances of objects (Dtos) defined by the Community
    Payback API for use in tests. It is better to always use one of these factories
    to setup test data as it reduces the number of breaking changes when Dto models
    change.
    
    These are copied over from Factories defined in the hmpps-community-payback-ui
    repository. I have copied them all with the exception of those for Dtos which
    are not currently defined on the schema for the Supervisor UI APIs.

---
+++ server/testutils/factories/appointmentSummaryFactory.ts
@@ -0,0 +1,15 @@
+import { Factory } from 'fishery'
+import { faker } from '@faker-js/faker'
+import { AppointmentSummaryDto } from '../../@types/shared'
+import offenderFullFactory from './offenderFullFactory'
+import contactOutcomeFactory from './contactOutcomeFactory'
+
+export default Factory.define<AppointmentSummaryDto>(() => ({
+  id: faker.number.int(),
+  startTime: '09:00',
+  endTime: '17:00',
+  contactOutcome: contactOutcomeFactory.build(),
+  requirementMinutes: faker.number.int(1000),
+  completedMinutes: faker.number.int(1000),
+  offender: offenderFullFactory.build(),
+}))
