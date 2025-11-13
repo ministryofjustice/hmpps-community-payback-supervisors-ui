d50fca2e483582ec8a92362a2901b49dd99f6322
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-13 12:06:28 +0000

    Remove `example-api` from Wiremock stubs
    We should remove the example client and example service too, but this gets our
    integration tests working with real requests to the Community Payback API.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 import { resetStubs } from './integration_tests/mockApis/wiremock'
 import auth from './integration_tests/mockApis/auth'
 import tokenVerification from './integration_tests/mockApis/tokenVerification'
-import exampleApi from './integration_tests/mockApis/exampleApi'
+import health from './integration_tests/mockApis/health'
 
 export default defineConfig({
   chromeWebSecurity: false,
@@ -20,7 +20,7 @@ export default defineConfig({
         reset: resetStubs,
         ...auth,
         ...tokenVerification,
-        ...exampleApi,
+        ...health,
       })
     },
     baseUrl: 'http://localhost:3007',
