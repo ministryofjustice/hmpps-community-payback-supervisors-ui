9e800938701b386d8978a1d00dae480d5729af57
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-14 14:03:24 +0000

    Add wiremock stub mapping for an appointment request

---
+++ integration_tests/mockApis/appointments.ts
@@ -0,0 +1,29 @@
+import { SuperAgentRequest } from 'superagent'
+import { stubFor } from './wiremock'
+import paths from '../../server/paths/api'
+import { AppointmentDto } from '../../server/@types/shared'
+
+export default {
+  stubFindAppointment: ({
+    appointment,
+    projectCode,
+  }: {
+    appointment: AppointmentDto
+    projectCode: string
+  }): SuperAgentRequest => {
+    const pattern = paths.appointments.singleAppointment({ projectCode, appointmentId: appointment.id.toString() })
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPathPattern: pattern,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {
+          ...appointment,
+        },
+      },
+    })
+  },
+}
