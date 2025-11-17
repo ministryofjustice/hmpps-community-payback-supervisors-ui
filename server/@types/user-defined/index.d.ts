c56fe936fa6c2010562708010e57c9d44c8c8b8b
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-17 10:33:36 +0000

    Add Validation errors type

--- server/@types/user-defined/index.d.ts
+++ server/@types/user-defined/index.d.ts
@@ -11,3 +11,5 @@ export interface GetAppointmentRequest extends BaseRequest {
   projectCode: string
   appointmentId: string
 }
+
+export type ValidationErrors<T> = Partial<Record<keyof T, Record<'text', string>>>
