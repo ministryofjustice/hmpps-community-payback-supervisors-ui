f63c6279d877eb9aba20df35792f03ec3b78097a
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-17 17:09:42 +0000

    Add integration test for submitting start time
    We need to stub an appointment outcome POST request, as we will save start time
    to delius immediately.

---
+++ integration_tests/pages/appointments/update/ableToWorkPage.ts
@@ -0,0 +1,8 @@
+import Page from '../../page'
+
+export default class AbleToWorkPage extends Page {
+  constructor() {
+    const title = 'Can Alex Boyle work today?'
+    super(title)
+  }
+}
--- integration_tests/tests/appointments/update/logStartTime.cy.ts
+++ integration_tests/tests/appointments/update/logStartTime.cy.ts
@@ -8,9 +8,13 @@
 //      Given I am on the start time page for an arrival form
 //      When I submit an invalid time
 //      Then I see the same page with errors
-//
+//    Scenario: Submitting a valid time
+//      Given I am on the start time page for an arrival form
+//      When I a submit a valid time
+//      Then I see the form next page
 
 import appointmentFactory from '../../../../server/testutils/factories/appointmentFactory'
+import AbleToWorkPage from '../../../pages/appointments/update/ableToWorkPage'
 import StartTimePage from '../../../pages/appointments/update/startTimePage'
 import Page from '../../../pages/page'
 
@@ -39,6 +43,24 @@ context('Log start time ', () => {
       Page.verifyOnPage(StartTimePage, appointment, true)
       page.shouldShowErrorSummary('startTime', 'Enter a start time')
       page.shouldHaveInputValue('startTime', '')
+    })
+
+    //  Scenario: Submitting a valid time
+    it('submits start time and navigates to next page', () => {
+      const projectCode = 'some-code'
+      const appointment = appointmentFactory.build({ startTime: '09:00' })
+      // Given I am on the start time page for an arrival form
+      cy.task('stubFindAppointment', { appointment, projectCode })
+
+      const page = StartTimePage.visit(appointment, projectCode, true)
+
+      // When I submit a valid time
+      cy.task('stubUpdateAppointmentOutcome', { appointment, projectCode })
+      page.enterStartTime('09:30')
+      page.clickSubmit()
+
+      // Then I see the next form page
+      Page.verifyOnPage(AbleToWorkPage)
     })
   })
 })
