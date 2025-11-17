f450d9e288cbd41a749df987efa60e0ab67ec09e
Dan Liburd <daniel.liburd@gmail.com>
2025-11-17 15:10:05 +0000

    Add integration test for viewing an appointment

--- integration_tests/pages/appointment.ts
+++ integration_tests/pages/appointment.ts
@@ -1,10 +1,37 @@
 import { AppointmentDto } from '../../server/@types/shared'
 import Offender from '../../server/models/offender'
+import SummaryListComponent from './components/summaryListComponent'
 import Page from './page'
+import paths from '../../server/paths'
 
 export default class AppointmentPage extends Page {
+  private readonly appointmentDetails: SummaryListComponent
+
+  private readonly appointment: AppointmentDto
+
   constructor(appointment: AppointmentDto) {
     const offender = new Offender(appointment.offender)
     super(offender.name)
+
+    this.appointment = appointment
+    this.appointmentDetails = new SummaryListComponent()
+  }
+
+  static visit(appointment: AppointmentDto): AppointmentPage {
+    cy.visit(
+      paths.appointments.show({ projectCode: appointment.projectCode, appointmentId: appointment.id.toString() }),
+    )
+
+    return new AppointmentPage(appointment)
+  }
+
+  shouldShowOffenderDetails(): void {
+    cy.get('span').contains(this.appointment.offender.crn)
+  }
+
+  shouldShowAppointmentDetails(): void {
+    this.appointmentDetails.getValueWithLabel('Session status').should('contain.text', 'Scheduled')
+    this.appointmentDetails.getValueWithLabel('Start time').should('contain.text', this.appointment.startTime)
+    this.appointmentDetails.getValueWithLabel('Finish time').should('contain.text', this.appointment.endTime)
   }
 }
