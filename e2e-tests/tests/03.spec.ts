39658d28aefb8815f8efe6f014b4daea2cbb5eff
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-19 13:04:12 +0000

    Add start of e2e test for recording absence
    This will be updated when the confirm page is added.

--- e2e-tests/tests/03.spec.ts
+++ e2e-tests/tests/03.spec.ts
@@ -3,10 +3,8 @@ import signIn from '../steps/signIn'
 import SessionPage from '../pages/sessionPage'
 import AppointmentPage from '../pages/appointmentPage'
 import StartTimePage from '../pages/appointments/update/startTimePage'
-import AbleToWorkPage from '../pages/appointments/update/ableToWorkPage'
-import ConfirmWorkingPage from '../pages/appointments/update/confirmWorkingPage'
 
-test('Record an arrival and log as working', async ({ page, deliusUser }) => {
+test('Record an absence', async ({ page, deliusUser }) => {
   const homePage = await signIn(page, deliusUser)
 
   await homePage.viewDetailsLinkLocator.click()
@@ -21,20 +19,12 @@ test('Record an arrival and log as working', async ({ page, deliusUser }) => {
   await appointmentPage.expect.toShowAppointmentDetails()
   await appointmentPage.expect.toShowOffenderDetails()
 
-  await appointmentPage.clickArrived()
+  await appointmentPage.clickNotArrived()
 
-  const startTimePage = new StartTimePage(page, 'arrived')
+  const startTimePage = new StartTimePage(page, 'absent')
   await startTimePage.expect.toBeOnThePage()
   await startTimePage.enterAStartTime()
   await startTimePage.clickContinue()
 
-  const ableToWorkPage = new AbleToWorkPage(page)
-  await ableToWorkPage.expect.toBeOnThePage()
-  await ableToWorkPage.checkYes()
-  await ableToWorkPage.clickContinue()
-
-  const confirmWorkingPage = new ConfirmWorkingPage(page)
-  await confirmWorkingPage.clickLinkToSessionPage()
-
-  await sessionPage.expect.toBeOnThePage()
+  await startTimePage.expect.toBeOnThePage()
 })
