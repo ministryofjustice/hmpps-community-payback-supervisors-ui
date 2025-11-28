b7774a62d8f9b7f23a4dfe0f7bc487de8125f391
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-28 17:28:11 +0000

    Add routes for clearing status data for a session
    Add:
    - path,
    - route handler
    - controller methods - these will return not found if feature flag is not enabled.

--- server/controllers/sessionsController.ts
+++ server/controllers/sessionsController.ts
@@ -6,6 +6,8 @@ import Offender from '../models/offender'
 import paths from '../paths'
 import AppointmentStatusService from '../services/appointmentStatusService'
 import AppointmentUtils from '../utils/appointmentUtils'
+import config from '../config'
+import { notFound } from '../utils/utils'
 
 export default class SessionsController {
   constructor(
@@ -48,6 +50,48 @@ export default class SessionsController {
           formattedLocation: LocationUtils.locationToParagraph(session.location),
         },
       })
+    }
+  }
+
+  confirmClearSession(): RequestHandler {
+    return async (_req: Request, res: Response) => {
+      if (config.flags.enableClearSessionStatuses) {
+        const { projectCode, date } = _req.params
+
+        const request = {
+          username: res.locals.user.username,
+          projectCode,
+          date: date.toString(),
+        }
+
+        const session = await this.sessionService.getSession(request)
+        const formattedDate = DateTimeFormats.isoDateToUIDate(session.date)
+        const title = `Are you sure you want to clear data for this session?`
+        const detail = `This will clear all of the appointment statuses saved for the ${session.projectName} session on ${formattedDate}`
+
+        return res.render('development/clearData', {
+          backLink: paths.sessions.show({ projectCode, date }),
+          confirmLink: paths.sessions.clearSessionStatuses({ projectCode, date }),
+          title,
+          detail,
+        })
+      }
+
+      return notFound(res)
+    }
+  }
+
+  clearSessions(): RequestHandler {
+    return async (_req: Request, res: Response) => {
+      if (config.flags.enableClearSessionStatuses) {
+        const { projectCode, date } = _req.params
+
+        await this.appointmentStatusService.clearStatusesForSession(projectCode, date, res.locals.user.name)
+
+        return res.redirect(paths.sessions.show({ projectCode, date }))
+      }
+
+      return notFound(res)
     }
   }
 }
