4cd51c9338a7b21aaf85051889140d8dee08360c
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-01 17:46:15 +0000

    Add GovUkRadioGroupUtils
    Taken from Community Payback UI.

---
+++ server/utils/GovUKFrontend/GovUkRadioGroup.ts
@@ -0,0 +1,31 @@
+import { GovUkRadioOption, YesOrNo } from '../../@types/user-defined'
+
+export default class GovUkRadioGroup {
+  static yesNoItems({ checkedValue }: { checkedValue?: boolean }): GovUkRadioOption[] {
+    const options = [
+      { text: 'Yes', value: 'yes' },
+      { text: 'No', value: 'no' },
+    ]
+
+    return options.map(option => ({
+      ...option,
+      checked: option.value === GovUkRadioGroup.determineCheckedValue(checkedValue),
+    }))
+  }
+
+  private static determineCheckedValue(value?: boolean): 'yes' | 'no' | null {
+    if (value === false) {
+      return 'no'
+    }
+
+    if (value === true) {
+      return 'yes'
+    }
+
+    return null
+  }
+
+  static valueFromYesOrNoItem(value?: YesOrNo): boolean {
+    return value === 'yes'
+  }
+}
