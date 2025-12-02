f3d84156be56e67f7cc7c1ebb5cd98d3bd19e195
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-02 16:58:34 +0000

    Update first e2e test with finish journey
    As we need to record a journey as 'working' to finish the session, it makes
    sense to combine these two. Anticipate we will have one e2e test for every
    route to a completed session (including completed sessions with a non-compliance).

--- e2e-tests/tests/01.spec.ts
+++ e2e-tests/tests/01.spec.ts
@@ -2,11 +2,34 @@ import test from '../test'
 import signIn from '../steps/signIn'
 import clearSessionData from '../steps/clearSessionData'
 import recordArrivalAbleToWork from '../steps/recordArrivalAbleToWork'
+import AppointmentPage from '../pages/appointmentPage'
+import EndTimePage from '../pages/appointments/update/endTimePage'
+import CompliancePage from '../pages/appointments/update/compliancePage'
+import ConfirmCompletedPage from '../pages/appointments/update/confirm/confirmCompletedPage'
 
 test('Record an appointment which starts and finishes on time', async ({ page, deliusUser }) => {
   await signIn(page, deliusUser)
   await clearSessionData(page)
 
   // record arrival able to work
-  await recordArrivalAbleToWork(page)
+  const sessionPage = await recordArrivalAbleToWork(page)
+
+  // record finished on time
+  await sessionPage.clickOnAnAppointment()
+  const appointmentPage = new AppointmentPage(page)
+  await appointmentPage.clickFinishSession()
+
+  const endTimePage = new EndTimePage(page, 'completed')
+  await endTimePage.expect.toBeOnThePage()
+  await endTimePage.clickContinue()
+
+  const compliancePage = new CompliancePage(page)
+  await compliancePage.enterComplianceDetails()
+  await compliancePage.clickContinue()
+
+  const confirmCompletedPage = new ConfirmCompletedPage(page)
+  await confirmCompletedPage.expect.toBeOnThePage()
+
+  await confirmCompletedPage.clickLinkToSessionPage()
+  await sessionPage.expect.toBeOnThePage()
 })
