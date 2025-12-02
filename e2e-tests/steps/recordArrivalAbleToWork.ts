ca194c6747c9690d718b033b208c79a7c636a8f8
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-02 16:50:37 +0000

    Create e2e test step for recording arrival
    This is in preparation for two e2e tests which have the same starting journey:
    
    1. Arrived, able to work & finished on time
    2. Arrived, able to work & left early

---
+++ e2e-tests/steps/recordArrivalAbleToWork.ts
@@ -0,0 +1,41 @@
+import { Page } from '@playwright/test'
+import SessionPage from '../pages/sessionPage'
+import HomePage from '../pages/homePage'
+import AppointmentPage from '../pages/appointmentPage'
+import StartTimePage from '../pages/appointments/update/startTimePage'
+import IsAbleToWorkPage from '../pages/appointments/update/isAbleToWorkPage'
+import ConfirmWorkingPage from '../pages/appointments/update/confirm/confirmWorkingPage'
+
+export default async (page: Page): Promise<SessionPage> => {
+  const homePage = new HomePage(page)
+  await homePage.viewDetailsLinkLocator.click()
+
+  const sessionPage = new SessionPage(page)
+  await sessionPage.expect.toBeOnThePage()
+  await sessionPage.expect.toShowSessionDetails()
+  await sessionPage.clickOnAnAppointment()
+
+  const appointmentPage = new AppointmentPage(page)
+  await appointmentPage.expect.toBeOnThePage()
+  await appointmentPage.expect.toShowAppointmentDetails()
+  await appointmentPage.expect.toShowOffenderDetails()
+
+  await appointmentPage.clickArrived()
+
+  const startTimePage = new StartTimePage(page, 'arrived')
+  await startTimePage.expect.toBeOnThePage()
+  await startTimePage.enterAStartTime()
+  await startTimePage.clickContinue()
+
+  const isAbleToWorkPage = new IsAbleToWorkPage(page)
+  await isAbleToWorkPage.expect.toBeOnThePage()
+  await isAbleToWorkPage.checkYes()
+  await isAbleToWorkPage.clickContinue()
+
+  const confirmWorkingPage = new ConfirmWorkingPage(page)
+  await confirmWorkingPage.expect.toBeOnThePage()
+  await confirmWorkingPage.clickLinkToSessionPage()
+
+  await sessionPage.expect.toBeOnThePage()
+  return sessionPage
+}
--- e2e-tests/tests/01.spec.ts
+++ e2e-tests/tests/01.spec.ts
@@ -1,42 +1,12 @@
 import test from '../test'
 import signIn from '../steps/signIn'
-import SessionPage from '../pages/sessionPage'
-import AppointmentPage from '../pages/appointmentPage'
-import StartTimePage from '../pages/appointments/update/startTimePage'
-import IsAbleToWorkPage from '../pages/appointments/update/isAbleToWorkPage'
-import ConfirmWorkingPage from '../pages/appointments/update/confirm/confirmWorkingPage'
 import clearSessionData from '../steps/clearSessionData'
+import recordArrivalAbleToWork from '../steps/recordArrivalAbleToWork'
 
-test('Record an arrival and log as working', async ({ page, deliusUser }) => {
-  const homePage = await signIn(page, deliusUser)
-
+test('Record an appointment which starts and finishes on time', async ({ page, deliusUser }) => {
+  await signIn(page, deliusUser)
   await clearSessionData(page)
-  await homePage.viewDetailsLinkLocator.click()
 
-  const sessionPage = new SessionPage(page)
-  await sessionPage.expect.toBeOnThePage()
-  await sessionPage.expect.toShowSessionDetails()
-  await sessionPage.clickOnAnAppointment()
-
-  const appointmentPage = new AppointmentPage(page)
-  await appointmentPage.expect.toBeOnThePage()
-  await appointmentPage.expect.toShowAppointmentDetails()
-  await appointmentPage.expect.toShowOffenderDetails()
-
-  await appointmentPage.clickArrived()
-
-  const startTimePage = new StartTimePage(page, 'arrived')
-  await startTimePage.expect.toBeOnThePage()
-  await startTimePage.enterAStartTime()
-  await startTimePage.clickContinue()
-
-  const isAbleToWorkPage = new IsAbleToWorkPage(page)
-  await isAbleToWorkPage.expect.toBeOnThePage()
-  await isAbleToWorkPage.checkYes()
-  await isAbleToWorkPage.clickContinue()
-
-  const confirmWorkingPage = new ConfirmWorkingPage(page)
-  await confirmWorkingPage.clickLinkToSessionPage()
-
-  await sessionPage.expect.toBeOnThePage()
+  // record arrival able to work
+  await recordArrivalAbleToWork(page)
 })
