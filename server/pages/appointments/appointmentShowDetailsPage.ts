ec1407f5926b2378626988c53595867e3297a676
Dan Liburd <daniel.liburd@gmail.com>
2025-12-04 16:46:13 +0000

    Add left early action to the show appointment details page

--- server/pages/appointments/appointmentShowDetailsPage.ts
+++ server/pages/appointments/appointmentShowDetailsPage.ts
@@ -38,7 +38,10 @@ export default class AppointmentShowDetailsPage {
     }
 
     if (appointmentStatus === 'Working') {
-      return [{ text: 'Finish session', href: paths.appointments.completed.endTime(appointmentPathParams) }]
+      return [
+        { text: 'Finish session', href: paths.appointments.completed.endTime(appointmentPathParams) },
+        { text: 'Left site early', href: paths.appointments.leftEarly.endTime(appointmentPathParams) },
+      ]
     }
 
     return []
