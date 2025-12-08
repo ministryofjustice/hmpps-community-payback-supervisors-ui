92267c75647996828121804f96462fcf3236b87a
Dan Liburd <daniel.liburd@gmail.com>
2025-12-08 13:06:47 +0000

    Introduce pageWithNotes for integration tests
    As left early reason and unable to work pages have notes, create a shared model
    for both to extend from.

--- integration_tests/pages/appointments/update/unableToWorkPage.ts
+++ integration_tests/pages/appointments/update/unableToWorkPage.ts
@@ -1,11 +1,9 @@
 import { AppointmentDto } from '../../../../server/@types/shared'
-import Page from '../../page'
 import paths from '../../../../server/paths'
 import Offender from '../../../../server/models/offender'
+import PageWithNotes from './base/pageWithNotes'
 
-export default class UnableToWorkPage extends Page {
-  private userInput: string
-
+export default class UnableToWorkPage extends PageWithNotes {
   constructor(appointment: AppointmentDto) {
     const offender = new Offender(appointment.offender)
     const title = `Why is ${offender.name} unable to work today?`
@@ -24,20 +22,5 @@ export default class UnableToWorkPage extends Page {
 
   selectSentHomeServiceIssues(): void {
     this.getInputByLabel('Attended - Sent Home (service issues)').check()
-  }
-
-  enterNotesWithCharacterLength(characterLength: number): void {
-    this.userInput = 'x'.repeat(characterLength)
-
-    // Use 'invoke' instead of 'type' for performance reasons
-    this.getInputByLabel('Add notes').invoke('val', this.userInput)
-  }
-
-  shouldShowSubmittedNotes(): void {
-    this.getInputByLabel('Add notes').should('have.value', this.userInput)
-  }
-
-  checkSensitiveInformation(): void {
-    this.getInputByLabel('This information is not to be shared with the person on probation').check()
   }
 }
