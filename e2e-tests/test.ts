f0937d3fd515f7698d1900f55014a34eef276d3a
dhruv.patel <dhruv.patel@justice.gov.uk>
2025-12-09 11:09:53 +0000

    CPB-387: Delius setup data for Supervisor E2E tests

--- e2e-tests/test.ts
+++ e2e-tests/test.ts
@@ -1,6 +1,7 @@
 import { test as base } from '@playwright/test'
 
 import { TestOptions } from './testOptions'
+import PersonOnProbation from './delius/personOnProbation'
 
 export default base.extend<TestOptions>({
   deliusUser: [
@@ -8,6 +9,30 @@ export default base.extend<TestOptions>({
       username: process.env.DELIUS_USERNAME as string,
       password: process.env.DELIUS_PASSWORD as string,
     },
+    { option: true },
+  ],
+  supervisorUser: [
+    {
+      username: process.env.SUPERVISOR_USERNAME as string,
+      password: process.env.SUPERVISOR_PASSWORD as string,
+    },
+    { option: true },
+  ],
+  team: [
+    {
+      provider: 'East of England',
+      name: 'Default Designated Transfer Team',
+    },
+    { option: true },
+  ],
+  testCount: Number(process.env.PW_TOTAL_TESTS),
+  canCreateNewPops: true,
+  existingPops: [
+    [
+      new PersonOnProbation('Kathleen', 'Greenholt', 'X970944'),
+      new PersonOnProbation('Milton', 'Predovic', 'X970945'),
+      new PersonOnProbation('Darren', 'Botsford', 'X970948'),
+    ],
     { option: true },
   ],
 })
