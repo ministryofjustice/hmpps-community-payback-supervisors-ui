59be8fc3b9da2ea8d8c54eddd64de721b5f1e12b
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-10 08:33:24 +0000

    Add reusable locators and assertions to compliance page model
    This will allow for different combinations of entering and validating form
    values to check the form is populated correctly for each scenario.

--- integration_tests/pages/appointments/update/compliancePage.ts
+++ integration_tests/pages/appointments/update/compliancePage.ts
@@ -1,10 +1,10 @@
-import { AppointmentDto } from '../../../../server/@types/shared'
-import { AppointmentCompletedAction } from '../../../../server/@types/user-defined'
+import { AppointmentDto, AttendanceDataDto } from '../../../../server/@types/shared'
+import { AppointmentCompletedAction, YesOrNo } from '../../../../server/@types/user-defined'
 import paths from '../../../../server/paths'
 import Page from '../../page'
 
 export default class CompliancePage extends Page {
-  constructor() {
+  constructor(private readonly appointment: AppointmentDto) {
     super('Log compliance')
   }
 
@@ -21,14 +21,62 @@ export default class CompliancePage extends Page {
 
     cy.visit(path)
 
-    return new CompliancePage()
+    return new CompliancePage(appointment)
   }
 
   completeForm(): void {
-    this.checkRadioByNameAndValue('hiVis', 'yes')
-    this.checkRadioByNameAndValue('workedIntensively', 'no')
-    this.checkRadioByNameAndValue('workQuality', 'GOOD')
-    this.checkRadioByNameAndValue('behaviour', 'UNSATISFACTORY')
-    this.getTextInputByIdAndEnterDetails('notes', 'Attendance notes')
+    this.selectHiVisValue()
+    this.selectWorkedIntensivelyValue()
+    this.selectWorkQualityValue()
+    this.behaviourOption().check()
+    this.enterNotes()
   }
+
+  selectHiVisValue() {
+    this.hiVisOption().check()
+  }
+
+  shouldHaveSelectedHiVisValue(value: YesOrNo = 'yes') {
+    this.hiVisOption(value).should('be.checked')
+  }
+
+  selectWorkedIntensivelyValue() {
+    this.workedIntensivelyOption().check()
+  }
+
+  shouldHaveSelectedWorkedIntensivelyValue(value: YesOrNo = 'yes') {
+    this.workedIntensivelyOption(value).check()
+  }
+
+  selectWorkQualityValue() {
+    this.workQualityOption().check()
+  }
+
+  shouldHaveSelectedWorkQualityValue(value: AttendanceDataDto['workQuality'] = 'GOOD') {
+    this.workQualityOption(value).should('be.checked')
+  }
+
+  shouldHaveSelectedBehavourValue(value: AttendanceDataDto['behaviour'] = 'UNSATISFACTORY') {
+    this.behaviourOption(value).should('be.checked')
+  }
+
+  enterNotes() {
+    this.notesField().type('Attendance notes')
+  }
+
+  shouldHaveEnteredNotes() {
+    this.notesField().should('have.value', 'Attendance notes')
+  }
+
+  private hiVisOption = (value: YesOrNo = 'yes') => this.getRadioByNameAndValue('hiVis', value)
+
+  private workedIntensivelyOption = (value: YesOrNo = 'no') => this.getRadioByNameAndValue('workedIntensively', value)
+
+  private workQualityOption = (value: AttendanceDataDto['workQuality'] = 'GOOD') =>
+    this.getRadioByNameAndValue('workQuality', value)
+
+  private behaviourOption = (value: AttendanceDataDto['behaviour'] = 'UNSATISFACTORY') =>
+    this.getRadioByNameAndValue('behaviour', value)
+
+  private notesField = () => this.getInputByLabel('Notes')
 }
