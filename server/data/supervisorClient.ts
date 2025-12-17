35e49885e32b82ed45033e3012782be74c1de6fe
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-17 14:06:29 +0000

    Add supervisor client to fetch supervisor info

---
+++ server/data/supervisorClient.ts
@@ -0,0 +1,18 @@
+import { RestClient, AuthenticationClient, asSystem } from '@ministryofjustice/hmpps-rest-client'
+import config from '../config'
+import logger from '../../logger'
+import paths from '../paths/api'
+import { SupervisorDto } from '../@types/shared'
+import { createQueryString } from '../utils/utils'
+
+export default class SupervisorClient extends RestClient {
+  constructor(authenticationClient: AuthenticationClient) {
+    super('sessionClient', config.apis.communityPaybackApi, logger, authenticationClient)
+  }
+
+  async find(username: string): Promise<SupervisorDto> {
+    const query = createQueryString({ username })
+    const path = paths.supervisors.supervisor({})
+    return (await this.get({ path, query }, asSystem(username))) as SupervisorDto
+  }
+}
