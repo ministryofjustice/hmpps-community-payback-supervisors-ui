67be21e045e2393a2599c20d0a4ecaed3b8df0d4
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-12-22 15:48:51 +0000

    Enhance logging during delius data setup
    As the delius data setup takes a while, we want to add more detailed logs to
    improve developer confidence that the test is not hanging.
    
    - Add console.log with offender and total offender count.
    - Wrap different delius steps in Playwright TestSteps, which gives a more
      detailed status of test progress in the console.
    
      e.g. the following is printed when the test is in the nested step of 'Create
      offender':
    
      `deliusData > Create and allocate person > Create offender`

--- e2e-tests/tests/delius.data.setup.ts
+++ e2e-tests/tests/delius.data.setup.ts
@@ -16,29 +16,34 @@ import PersonOnProbation from '../delius/personOnProbation'
 test('deliusData', async ({ page, team, testCount, canCreateNewPops, existingPops }) => {
   slow()
   await login(page)
-  const upwProject = await createUpwProject(page, {
-    providerName: team.provider,
-    teamName: team.name,
+
+  const upwProject = await test.step('Create UPW project', async () => {
+    return createUpwProject(page, {
+      providerName: team.provider,
+      teamName: team.name,
+    })
   })
 
   const deliusTestData: DeliusTestData = {
     project: { name: upwProject.projectName, code: upwProject.projectCode },
     pops: [],
   }
-  const allocationPromises: Array<() => Promise<PersonOnProbation>> = Array.from({ length: testCount }, (_, i) => {
-    return () => createAndAllocatePerson(page, deliusTestData, team, existingPops, canCreateNewPops, i)
-  })
-  const personOnProbationResults = await allocationPromises.reduce<Promise<PersonOnProbation[]>>(
-    async (allocationPromiseChain, task) => {
-      const popList = await allocationPromiseChain
-      const pop = await task()
-      popList.push(pop)
-      return popList
-    },
-    Promise.resolve([]),
-  )
-
-  deliusTestData.pops.push(...personOnProbationResults)
+  /* eslint-disable no-await-in-loop */
+  for (let i = 0; i < testCount; i += 1) {
+    await test.step(`Create and allocate person ${i + 1} of ${testCount}`, async () => {
+      console.log('----- Creating and allocating person %d out of %d -----', i + 1, testCount) // eslint-disable-line  no-console
+      const personOnProbation = await createAndAllocatePerson(
+        page,
+        deliusTestData,
+        team,
+        existingPops,
+        canCreateNewPops,
+        i,
+      )
+      deliusTestData.pops.push(personOnProbation)
+    })
+  }
+  /* eslint-enable no-await-in-loop */
   await writeDeliusData(deliusTestData)
 })
 
@@ -53,28 +58,38 @@ async function createAndAllocatePerson(
   let pop: PersonOnProbation
   if (canCreateNewPops || existingPops.length < count) {
     const person = deliusPerson()
-    const crn: string = await createOffender(page, {
-      person,
-      providerName: team.provider,
+
+    const crn: string = await test.step('Create offender', async () => {
+      return createOffender(page, {
+        person,
+        providerName: team.provider,
+      })
     })
     pop = new PersonOnProbation(person.firstName, person.lastName, crn)
   } else {
     pop = existingPops[count]
   }
-  await createCommunityEvent(page, { crn: pop.crn, allocation: { team } })
+  await test.step('Create community event', async () => {
+    await createCommunityEvent(page, { crn: pop.crn, allocation: { team } })
+  })
 
-  await createRequirementForEvent(page, {
-    crn: pop.crn,
-    requirement: data.requirements.unpaidWork,
-    team,
+  await test.step('Create requirement for event', async () => {
+    await createRequirementForEvent(page, {
+      crn: pop.crn,
+      requirement: data.requirements.unpaidWork,
+      team,
+    })
   })
 
   await page.locator('a', { hasText: 'Personal Details' }).click()
 
-  await allocateCurrentCaseToUpwProject(page, {
-    providerName: team.provider,
-    teamName: team.name,
-    projectName: deliusTestData.project.name,
+  await test.step('Allocate person to project', async () => {
+    await allocateCurrentCaseToUpwProject(page, {
+      providerName: team.provider,
+      teamName: team.name,
+      projectName: deliusTestData.project.name,
+    })
   })
+
   return pop
 }
