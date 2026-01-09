37754ab7e91735ba4763148e209e4065b5a0bc6d
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-01-09 12:17:23 +0000

    Fix mechanism to determine unique person to use in test
    In 08835d7 I added a dynamic index to each test, to help select a unique Person
    on Probation to use for each test from the setup data. Unfortunately, this
    didn't work as it relied on a parallel worker index, and we are only using one worker.
    
    This builds on the count reporter 'testCount' mechanism, and also stores a list
    of testIds to enable each test to create an index based on its order in the
    test run.

--- e2e-tests/fixtures/appointmentTest.ts
+++ e2e-tests/fixtures/appointmentTest.ts
@@ -5,7 +5,8 @@ import { AppointmentTestOptions } from './testOptions'
 export default test.extend<AppointmentTestOptions>({
   // eslint-disable-next-line no-empty-pattern
   testData: async ({}, use) => {
-    const index = test.info().parallelIndex
+    const testIds = process.env.PW_TESTS?.split(',')
+    const index = testIds.findIndex(id => id === test.info().testId)
     const testData = await readDeliusData(index)
     await use(testData)
   },
