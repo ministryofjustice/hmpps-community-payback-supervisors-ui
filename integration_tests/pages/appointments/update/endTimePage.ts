fd15eff4e63c036ef23eb813f24334ae85ab5287
Patrick Fleming <patrick.fleming@dxw.com>
2026-04-02 11:25:35 +0100

    Update end time page so next page arrived journey is unable to work page
    I’ve also updated the page title for this journey.

--- integration_tests/pages/appointments/update/endTimePage.ts
+++ integration_tests/pages/appointments/update/endTimePage.ts
@@ -1,27 +1,35 @@
 import { AppointmentDto } from '../../../../server/@types/shared'
 import Offender from '../../../../server/models/offender'
 import paths from '../../../../server/paths'
-import { AppointmentCompletedAction } from '../../../../server/@types/user-defined'
+import { AppointmentEndTimeAction } from '../../../../server/@types/user-defined'
 import LogTimePage from './base/logTimePage'
 
 export default class EndTimePage extends LogTimePage {
-  constructor(appointment: AppointmentDto, action: AppointmentCompletedAction) {
+  constructor(appointment: AppointmentDto, action: AppointmentEndTimeAction) {
     const offender = new Offender(appointment.offender)
     const title: string = EndTimePage.getExpectedTitle(action, offender)
     super(title)
   }
 
-  static visit(appointment: AppointmentDto, action: AppointmentCompletedAction): EndTimePage {
-    const path = paths.appointments[action].endTime({
-      appointmentId: appointment.id.toString(),
-      projectCode: appointment.projectCode,
-    })
+  static visit(appointment: AppointmentDto, action: AppointmentEndTimeAction): EndTimePage {
+    let path: string
+    if (action === 'arrived') {
+      path = paths.appointments.arrived.endTime({
+        appointmentId: appointment.id.toString(),
+        projectCode: appointment.projectCode,
+      })
+    } else {
+      path = paths.appointments[action].endTime({
+        appointmentId: appointment.id.toString(),
+        projectCode: appointment.projectCode,
+      })
+    }
     cy.visit(path)
 
     return new EndTimePage(appointment, action)
   }
 
-  private static getExpectedTitle(action: AppointmentCompletedAction, offender: Offender) {
+  private static getExpectedTitle(action: AppointmentEndTimeAction, offender: Offender) {
     let title: string
 
     if (action === 'completed') {
@@ -30,6 +38,10 @@ export default class EndTimePage extends LogTimePage {
 
     if (action === 'leftEarly') {
       title = `You're logging out ${offender.name} early today at:`
+    }
+
+    if (action === 'arrived') {
+      title = `You're logging ${offender.name} as having left at:`
     }
     return title
   }
