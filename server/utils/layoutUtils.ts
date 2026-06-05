39ce27258b7079663fbfa6e718d6783e983e8933
Ben Shimmin <bas@bas.me.uk>
2026-06-05 10:24:18 +0100

    Remove LayoutUtils
    We no longer need this because we're not adding links into the footer,
    except for the "Clear session data" link, which will also eventually be
    removed.

--- server/utils/layoutUtils.ts
+++
@@ -1,7 +0,0 @@
-import { LinkItem } from '../@types/user-defined'
-
-export default class LayoutUtils {
-  static getFooterItems(footerLinks: LinkItem[] = []): LinkItem[] {
-    return footerLinks
-  }
-}
